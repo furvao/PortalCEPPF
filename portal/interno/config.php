225b7987018593d5e3a5871641358e9e1265785e
Fulvio Almeida Silva <fulvioalmeidasilva@gmail.com>
2017-06-03 11:06:12 -0300

    Added new architecture of portal based in necessity and complexity that application needs. The portal was deployed at http://ceppf.byethost5.com/ using bootstrap and tool of management course using moodle.

---
+++ portal/interno/config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'id1521689_moodle';
+$CFG->dbuser    = 'id1521689_root';
+$CFG->dbpass    = 'fas150591';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'https://ceppf.byethost5.com/interno/';
+$CFG->dataroot  = '/storage/h13/689/1521689/public_html/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
